e6852542d564874a38cc987997bdda44fcfc055a
Shrijan Maharjan <srijinx@gmail.com>
2021-09-03 23:04:11 +0545

    hello

---
+++ client.js
@@ -0,0 +1,19 @@
+console.log('hello world!');
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event)=> {
+    console.log('form is submitted');
+    const formData = new FormData(form)
+    const name = formData.get('name');
+    const content= formData.get('content');
+    const mew={
+        name,
+        content
+    }
+console.log(mew);
+form.style.display = 'none';
+loadingElement.style.display ='';
+})
